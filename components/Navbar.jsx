1ba3701dd67b793ef0a57eecbf589d166da3ff26
akinyiliz <lizzieke84@gmail.com>
2023-05-01 15:10:40 +0300

    Created Navbar component

---
+++ components/Navbar.jsx
@@ -0,0 +1,103 @@
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+
+function Navbar() {
+	const [nav, setNav] = useState(false);
+	const [color, setColor] = useState("transparent");
+	const [textColor, setTextColor] = useState("white");
+
+	const handleNav = () => {
+		setNav(!nav);
+	};
+
+	useEffect(() => {
+		const changeColor = () => {
+			if (window.scrollY >= 90) {
+				setColor("#ffffff");
+				setTextColor("#000000");
+			} else {
+				setColor("transparent");
+				setTextColor("#ffffff");
+			}
+		};
+		window.addEventListener("scroll", changeColor);
+	}, []);
+
+	return (
+		<div
+			style={{ backgroundColor: `${color}` }}
+			className="fixed left-0 top-0 w-full z-10 ease-in duration-300"
+		>
+			<div className="max-w-[1240px] m-auto flex justify-between items-center p-4 text-white">
+				<Link
+					href="/"
+					style={{ color: `${textColor}` }}
+				>
+					<h1 className="font-bold text-4xl">CreativePhoto</h1>
+				</Link>
+				<ul
+					style={{ color: `${textColor}` }}
+					className="hidden sm:flex"
+				>
+					<li className="p-4">
+						<Link href="/">Home</Link>
+					</li>
+					<li className="p-4">
+						<Link href="/#gallery">Gallery</Link>
+					</li>
+					<li className="p-4">
+						<Link href="/portfolio">Work</Link>
+					</li>
+					<li className="p-4">
+						<Link href="/contact">Contact</Link>
+					</li>
+				</ul>
+
+				{/* Mobile Button */}
+				<div
+					onClick={handleNav}
+					className="block sm:hidden z-10"
+				>
+					{nav ? (
+						<AiOutlineClose
+							size={25}
+							style={{ color: `${textColor}` }}
+						/>
+					) : (
+						<AiOutlineMenu
+							size={25}
+							style={{ color: `${textColor}` }}
+						/>
+					)}
+				</div>
+
+				{/* Mobile Menu */}
+				<div
+					className={
+						nav
+							? "sm:hidden absolute top-0 right-0 left-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300"
+							: "sm:hidden absolute top-0 right-0 left-[-100%] bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300"
+					}
+				>
+					<ul>
+						<li className="p-4 text-4xl hover:text-gray-500">
+							<Link href="/">Home</Link>
+						</li>
+						<li className="p-4 text-4xl hover:text-gray-500">
+							<Link href="/#gallery">Gallery</Link>
+						</li>
+						<li className="p-4 text-4xl hover:text-gray-500">
+							<Link href="/portfolio">Work</Link>
+						</li>
+						<li className="p-4 text-4xl hover:text-gray-500">
+							<Link href="/contact">Contact</Link>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Navbar;
