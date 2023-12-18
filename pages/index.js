39336fae3fd89bfd88b9a5bf440b8460ba23783d
akinyiliz <lizzieke84@gmail.com>
2023-12-18 18:36:55 +0300

    fix: Remove slide prop

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,21 @@
-import Hero from "../components/Hero";
 import Head from "next/head";
+
+import Hero from "../components/Hero";
 import Slider from "@/components/Slider";
-import { SliderData } from "@/components/SliderData";
 import Instagram from "@/components/Instagram";
 
 export default function Home() {
-	return (
-		<div>
-			<Head>
-				<title>Photography Portfolio</title>
-			</Head>
-			<Hero
-				heading="Creative Photography"
-				message="Capturing moments, preserving memories - one shot at a time."
-			/>
-			<Slider slides={SliderData} />
-			<Instagram />
-		</div>
-	);
+  return (
+    <div>
+      <Head>
+        <title>Photography Portfolio</title>
+      </Head>
+      <Hero
+        heading="Creative Photography"
+        message="Capturing moments, preserving memories - one shot at a time."
+      />
+      <Slider />
+      <Instagram />
+    </div>
+  );
 }
