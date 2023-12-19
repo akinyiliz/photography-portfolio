41957daa22dcc4fe76482ce037455dfccb07f7ae
akinyiliz <lizzieke84@gmail.com>
2023-12-19 07:52:19 +0300

    feat: Add Footer component

--- pages/work.jsx
+++ pages/work.jsx
@@ -1,17 +1,17 @@
-import Hero from "@/components/Hero";
-import Portfolio from "@/components/Portfolio";
 import React from "react";
 
+import Hero from "@/components/Hero";
+import Portfolio from "@/components/Portfolio";
+import Footer from "@/components/Footer";
+
 const work = () => {
-	return (
-		<div>
-			<Hero
-				heading="My Work"
-				message="This is some of my recent work."
-			/>
-			<Portfolio />
-		</div>
-	);
+  return (
+    <div>
+      <Hero heading="My Work" message="This is some of my recent work." />
+      <Portfolio />
+      <Footer />
+    </div>
+  );
 };
 
 export default work;
