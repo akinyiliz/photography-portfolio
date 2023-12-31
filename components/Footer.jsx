f372565757072565ab554bf44443b197a9b09a21
akinyiliz <lizzieke84@gmail.com>
2023-12-31 14:41:27 +0300

    chore: Update year

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
-    <footer class="bg-black/80 p-6 flex items-center justify-center">
+    <footer className="bg-black/80 p-6 flex items-center justify-center">
       <p className="text-white">
-        &copy;2023 CreativePhoto. All Rights Reserved.
+        &copy;2024 CreativePhoto. All Rights Reserved.
       </p>
     </footer>
   );
